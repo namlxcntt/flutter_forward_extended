98f5709a5ca6df26fb1477260472b887260273fc
namlx <luongxuannam@cryptopie-labo.com>
2023-03-15 21:27:21 +0700

    Create features

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -6,6 +6,5 @@ class Assets {
   static const String iconsTabOne = 'assets/icons/tab_one.svg';
   static const String iconsTabThree = 'assets/icons/tab_three.svg';
   static const String iconsTabTwo = 'assets/icons/tab_two.svg';
-  static const String imagesTabOne = 'assets/images/tab_one.png';
 
 }
